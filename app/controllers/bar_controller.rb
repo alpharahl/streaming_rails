9c0ab67e5c25bf2a2e8e260e1710b82ce020d41a
David Gauntlett <david.m.gauntlett@gmail.com>
2018-06-28 00:42:07 -0400

    Support proper state storage

--- app/controllers/bar_controller.rb
+++ app/controllers/bar_controller.rb
@@ -2,21 +2,10 @@ require 'json'
 
 class BarController < ApplicationController
   def create
-    cells = params[:blob]
-    cells.each do |y, x_array|
-      x_array.each_with_index do |x, indx|
-        cell = Cell.where(x: indx, y: y).first
-        if cell
-          cell.update_attributes({walkable: x})
-        else
-          BarHelper.createCell({
-            x: indx,
-            y: y,
-            walkable: x
-          })
-        end
-      end
-    end
-    render json: cells
+    d = Datum.first
+    Rails.logger.info(params[:blob])
+    d.blob = params[:blob].to_json
+    d.save!
+    head :no_content
   end
 end
--- app/controllers/foo_controller.rb
+++ app/controllers/foo_controller.rb
@@ -2,21 +2,6 @@ require 'json'
 
 class FooController < ApplicationController
   def index
-    # j = {}
-    # 100.times{|y|
-    #   a = []
-    #   98.times{a << [true,false].sample}
-    #   j[y] = a
-    # }
-    # render json: j.to_json
-    ys = Cell.pluck(:y).uniq
-    j = {}
-    ys.each do |y|
-      j[y] = Cell
-        .group(:x)
-        .where(y: y)
-        .select(:x, :walkable)
-    end
-    render json: j.to_json
+    render json: Datum.first.blob
   end
 end
